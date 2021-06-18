ebe384c430d406ba9f581d339b59d6db64f9b96a
Tamiris Fonseca <tamirissf@live.com>
2021-06-17 21:17:41 -0300

    Updates

---
+++ precache-manifest.dd4448b639c84368a4314e375271e1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb79ba6727ca73992a95e3a0a0ca2ea2",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "c556d0c55b4234b6e277",
+    "url": "/2048/static/css/main.4bb90227.chunk.css"
+  },
+  {
+    "revision": "fa0c2728d87c7218bf96",
+    "url": "/2048/static/js/2.3fbc8cd6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/2048/static/js/2.3fbc8cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c556d0c55b4234b6e277",
+    "url": "/2048/static/js/main.8df75092.chunk.js"
+  },
+  {
+    "revision": "2872567e080dbff5017e",
+    "url": "/2048/static/js/runtime-main.acc8e921.js"
+  }
+]);
